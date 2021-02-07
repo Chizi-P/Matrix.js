43de8755d325a29d8be7345d823c46865adbabd6
Chizi-P <61510435+Chizi-P@users.noreply.github.com>
2021-02-07 18:43:20 +0800

    Update Matrix.js

--- Matrix.js
+++ Matrix.js
@@ -4,15 +4,19 @@
  */
 
 
-Object.prototype.safeDefineProperty = Object.prototype.safeDefineProperty ?? function(obj, prop, descriptor) {
+Object.prototype.safeDefineProperty = Object.prototype.safeDefineProperty ?? function safeDefineProperty(obj, prop, descriptor) {
     if (obj[prop]) throw `${obj.constructor.name} 的 ${prop} 已存在！`;
     return Object.defineProperty(obj, prop, descriptor);
 }
 
-// 無關聯複製
-Array.prototype.copy = function() {
-    return JSON.parse(JSON.stringify(this));
-}
+// 淺拷貝
+Object.safeDefineProperty(Array.prototype, 'shallowCopy', {
+    value: () => this.slice()
+});
+// 深拷貝（無關聯）
+Object.safeDefineProperty(Array.prototype, 'deepCopy', {
+    value: () => JSON.parse(JSON.stringify(this))
+});
 
 
 class StaticMatrix {
@@ -21,8 +25,14 @@ class StaticMatrix {
         const w = obj[0]?.length;
         return obj.every?.(e => e.length === w) ?? false;
     }
-    static isSquareMatrix(obj) {
+    static isSquare(obj) {
         return obj.isMatrix() && obj.length === obj[0].length;
+    }
+    static isSameSize(obj1, obj2) {
+        if (obj1 instanceof Matrix && obj2 instanceof Matrix) {
+            return obj1.height === obj2.height && obj1.width === obj2.width;
+        
+        } else throw '參數應為 Matrix 類別';
     }
 }
 
@@ -32,12 +42,11 @@ class Matrix extends StaticMatrix {
         let matrix;
         if (Array.isArray(height)) {
             if (Matrix.isMatrix(height)) {
-                matrix = height;
+                matrix = height.deepCopy();
                 this._height = matrix.length;
                 this._width = matrix[0].length;
-            }
 
-            else throw '傳入了非矩陣';
+            } else throw '傳入了非矩陣';
 
         } else if (Number.isInteger(height + width)) {
             // _不被調用的屬性
@@ -74,18 +83,87 @@ class Matrix extends StaticMatrix {
     get width() {
         return this._width;
     }
+
+    /** 
+     * Basic operations
+     * 基本運算
+     */
+    
+    forAll(currentValue, indexI, indexJ, ) {
+        for (let i = 0; i < this.height; i++) {
+            for (let j = 0; j < this.width; j++) {
+                matrix[i][j] = this[i][j] + value[i]?.[j] ?? value;
+            }
+        }
+    }
+
+    plus(value = 0) {
+        if (value instanceof Matrix) {
+            if (!Matrix.isSameSize(this, value)) {
+                throw '矩陣不同大小，無法相加';
+            }
+        }
+        let matrix = new Matrix(this.height, this.width);
+        for (let i = 0; i < this.height; i++) {
+            for (let j = 0; j < this.width; j++) {
+                matrix[i][j] = this[i][j] + value[i]?.[j] ?? value;
+            }
+        }
+        return matrix;
+    }
+    minus(value = 0) {
+        
+    }
+    multiply() {
+
+    }
+    divide() {
+
+    }
+    
     /**
      * @param {any} value
      */
     init(value) {
-        for (let i = 0, l1 = this.length; i < l1; i++) {
-            for (let j = 0, l2 = this[0].length; j < l2; j++) {
+        for (let i = 0; i < this.height; i++) {
+            for (let j = 0; j < this.width; j++) {
                 this[i][j] = value;
             }
         }
         return this;
     }
+    reshape(row, column) {
+        if (this.height * this.width != row * column) {
+            throw '此參數值的長寬無法重塑矩陣';
+        }
+        let matrix = new Matrix(row, column);
+        // const iterator = this.flat().entries();
+        const temp = this.flat();
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < column; j++) {
+                  matrix[i][j] = temp[i + j * row];
+            }
+        }
+        return matrix;
+    }
+    
 }
 // Matrix 繼承 Array 的方法和迭代器
 Matrix.prototype.__proto__ = Array.prototype;
 
+function executionTime(func) {
+    const t0 = new Date().getTime();
+    func();
+    const t1 = new Date().getTime();
+    console.log(`執行了 ${t1 - t0} 毫秒`);
+    return t1 - t0;
+}
+
+
+let A = new Matrix(2, 3, 1);
+let B = new Matrix(2, 3, 0);
+console.log(A);
+executionTime(() => {
+    console.log(A.plus(B.init(3)))
+})
+console.log(A)
